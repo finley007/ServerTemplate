2e3cd9a1f2773f678506c7fe004753ea621e1de8
finley007 <finley916@gmail.com>
2016-11-03 10:25:36 +0800

    Add request and response base class

---
+++ java-template/src/main/java/com/template/request/Request.java
@@ -0,0 +1,7 @@
+package com.template.request;
+
+/**
+ * Created by liuli on 11/3/2016.
+ */
+public class Request {
+}
---
+++ java-template/src/main/java/com/template/response/Response.java
@@ -0,0 +1,7 @@
+package com.template.response;
+
+/**
+ * Created by liuli on 11/3/2016.
+ */
+public class Response {
+}
